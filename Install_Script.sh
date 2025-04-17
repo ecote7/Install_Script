29773fa3455fad8677d656f4bf7b1b6358e96576
ECote <etienne.cote@gmail.com>
2025-04-17 15:14:17 -0400

    Update Install_Script.sh

--- Install_Script.sh
+++ Install_Script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt install make net-tools plocate proxychains wafw00f socat masscan nmap python3 python3-pip python3-venv python3-impacket python3-pycryptodome python3-typer python3-tabulate python3-pandas python3-requests-toolbelt python3-requests-ntlm python3-cmd2 python3-ldap3 smbclient sqlmap ruby-full build-essential libffi-dev libssl-dev libldap2-dev libsasl2-dev libkrb5-dev libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev zlib1g zlib1g-dev libpq-dev libpcap-dev libsqlite3-dev ldap-utils libffi-dev pipx git zip unzip faketime libfdt1 libpixman-1-0 libpng16-16 liboss4-salsa-asound2 libglib2.0-0 autoconf bison libncurses5-dev libreadline-dev libyaml-dev libgdbm-dev -y
 
 mkdir /home/ubuntu/tools
@@ -12,22 +14,26 @@ rm ffuf_2.1.0_linux_amd64.tar.gz CHANGELOG.md LICENSE README.md
 cd /home/ubuntu/tools
 wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.24.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go
+export GOCACHE=$HOME/.cache/go-build
+export PATH=$PATH:$GOPATH/bin
+echo "Go version: $(go version)"
 echo 'export PATH=$PATH:/usr/local/go/bin' | tee -a /etc/profile.d/golang.sh
 echo 'export GOPATH=$HOME/go' | tee -a /etc/profile.d/golang.sh
 echo 'export GOCACHE=$HOME/.cache/go-build' | tee -a /etc/profile.d/golang.sh
 echo 'export PATH=$PATH:$GOPATH/bin' | tee -a /etc/profile.d/golang.sh
 chmod 644 /etc/profile.d/golang.sh
 rm go1.24.2.linux-amd64.tar.gz
-. /etc/profile
 go install github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
 cp /go/bin/nuclei /usr/local/bin/
 
-git clone https://github.com/lgandx/Responder.git
-git clone https://github.com/drwetter/testssl.sh.git
-git clone https://github.com/internetwache/GitTools.git
+#git clone https://github.com/lgandx/Responder.git
+#git clone https://github.com/drwetter/testssl.sh.git
+#git clone https://github.com/internetwache/GitTools.git
 #git clone https://github.com/danielmiessler/SecLists.git
-git clone https://github.com/aboul3la/Sublist3r.git
-git clone https://github.com/dirkjanm/BloodHound.py
+#git clone https://github.com/aboul3la/Sublist3r.git
+#git clone https://github.com/dirkjanm/BloodHound.py
 git clone https://github.com/trufflesecurity/trufflehog.git
 cd trufflehog; go install -buildvcs=false
 
